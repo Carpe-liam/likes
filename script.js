2268b9f6d9326b0e92cd76f538683fe281648f08
Alex Tyler <alexander.liam.tyler@gmail.com>
2021-08-19 14:55:06 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var neil = 9;
+var neilLikes = document.querySelector(".neil");
+function addNeil(){
+    neil++;
+    neilLikes.innerText = neil + "like(s)";
+}
+
+var nichole = 12;
+var nicholeLikes = document.querySelector(".nichole");
+function addNichole(){
+    nichole++;
+    nicholeLikes.innerText = nichole + "like(s)";
+}
+
+var jim = 9;
+var jimLikes = document.querySelector(".jim");
+function addJim(){
+    jim++;
+    jimLikes.innerText = jim + "like(s)";
+}
